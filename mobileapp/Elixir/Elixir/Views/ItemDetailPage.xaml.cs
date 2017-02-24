c6fe1dadb0a542c6c556ed55ebe454b06c8e92fe
Brandon H <hurlburb@microsoft.com>
2017-02-23 22:08:24 -0800

    initial xam.forms sln creation

---
+++ mobileapp/Elixir/Elixir/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using Elixir.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Elixir.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
