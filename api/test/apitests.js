d978ed8b52d4c8561f4eea005d0d667b0c6ccc6d
jamesbak@microsoft.com <jamesbak@microsoft.com>
2017-02-28 23:09:54 -0800

    Unit test diagnostics

--- api/test/apitests.js
+++ api/test/apitests.js
@@ -32,9 +32,11 @@ function getAccessToken(refreshToken, next) {
 before((done) => {
     getAccessToken(process.env.AdminRefreshToken, (err, token) => {
         if (token) {
+            console.log('Admin token: ' + token);
             adminBearerToken = token;
             getAccessToken(process.env.NonAdminRefreshToken, (err, token) => {
                 if (token) {
+                    console.log('Non-admin token: ' + token);
                     nonAdminBearerToken = token;
                     done();
                 }
--- api/test/apitests.ts
+++ api/test/apitests.ts
@@ -36,9 +36,11 @@ before((done) => {
     // Fetch bearer token using refresh token specified in env vars
     getAccessToken(process.env.AdminRefreshToken, (err, token) => {
         if (token) {
+            console.log('Admin token: ' + token);
             adminBearerToken = token;
             getAccessToken(process.env.NonAdminRefreshToken, (err, token) => {
                 if (token) {
+                    console.log('Non-admin token: ' + token);
                     nonAdminBearerToken = token;
                     done();
                 }
