86b5c0217e9fc1f76c7729a7c36fa1ff283c8135
jamesbak@microsoft.com <jamesbak@microsoft.com>
2017-04-26 15:07:33 -0700

    Update deployment failure error message

--- deploy.cmd
+++ deploy.cmd
@@ -8,7 +8,7 @@ IF "%SITE_TYPE%" == "webapp" (
     IF "%SITE_TYPE%" == "app" (
       deploy.app.cmd
     ) ELSE (
-      echo You have to set SITE_TYPE setting to either "webapp" or "api"
+      echo You have to set SITE_TYPE setting to either "webapp", "api" or "app"
       exit /b 1
     )
   )
